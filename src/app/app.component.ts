e09d4f998f585a78d1d0a413e59fafc4af3c8bb6
Alberto Basalo <albertobasalo@hotmail.com>
2022-03-01 17:46:08 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stk-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'softtek-angular-avanzado';
+}

1e54f8bd6f12ccd9786d4705ad9046818de26a45
Alberto Basalo <albertobasalo@hotmail.com>
2022-03-03 17:15:18 +0100

    feat: template ref context outlet

--- src/app/shared/components/async-wrapper/async-wrapper.component.ts
+++ src/app/shared/components/async-wrapper/async-wrapper.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit, TemplateRef } from '@angular/core';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Component({
@@ -8,8 +8,9 @@ import { BehaviorSubject, Observable, tap } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AsyncWrapperComponent implements OnInit {
-  private _data$!: Observable<unknown>;
+  @Input() dataTemplate!: TemplateRef<HTMLElement>;
 
+  private _data$!: Observable<unknown>;
   @Input() set data$(value: Observable<unknown>) {
     if (!value) return;
     this._data$ = value.pipe(
@@ -21,6 +22,7 @@ export class AsyncWrapperComponent implements OnInit {
   get data$(): Observable<unknown> {
     return this._data$;
   }
+
   error$ = new BehaviorSubject('');
   constructor() {}
 
